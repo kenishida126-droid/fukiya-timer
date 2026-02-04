f6c74eda8ebfcb3e5091bca990bdf6912a6fe599
kenishida126-droid <ken.ishida126@gmail.com>
2026-02-04 10:06:46 +0900

    Delete x_service-worker.js

--- x_service-worker.js
+++ x_service-worker.js
@@ -1,12 +1,1 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
 
